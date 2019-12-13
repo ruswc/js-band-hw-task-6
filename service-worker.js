e5b073df94ea9361298d16a413a3c18389106572
ruswc (via Travis CI) <44748420+ruswc@users.noreply.github.com>
2019-12-13 11:45:01 +0000

    Deploy ruswc/js-band-hw-task-6 to github.com/ruswc/js-band-hw-task-6.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.948184b8f5dbf5db8e121129a813019b.js"
+  "/js-band-hw-task-6/precache-manifest.064200f382994feb9ab6cbeda0aece85.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/js-band-hw-task-6/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
